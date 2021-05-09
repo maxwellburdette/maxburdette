e06909be867f77d419c0a0c7fd776dbda9d3e767
maxwellburdette <maxbdevelops@gmail.com>
2021-05-09 16:19:43 -0500

    Added job description

--- src/sections/About.js
+++ src/sections/About.js
@@ -5,7 +5,6 @@ export default function About() {
 		<div className="about">
 			<h2 className="title2">Profile</h2>
 			<p>
-				Graduated with Bachelors degree in Computer Science on April 28th, 2021.
 				Seeking career in field of Software Engineering. Excel in all areas of
 				the development process with special interest in frontend development.
 			</p>
--- src/sections/Experience.js
+++ src/sections/Experience.js
@@ -11,7 +11,36 @@ export default function Experience() {
 				</div>
 				<div className="text">
 					<h4>Advanced Repair Agent</h4>
-					<p>Add description later</p>
+					<p>
+						<ul>
+							<li>
+								Resolve complex software and hardware repairs efficiently with
+								top-level accuracy.
+							</li>
+							<li>
+								Complete in-depth malware removal and operating system
+								restoration including BSOD.
+							</li>
+							<li>
+								Complete wide array of repairs on iOS and MacOS devices
+								consistent with Apple and ITC standards.
+							</li>
+							<li>
+								Ensure client satisfaction and QoS through accurate
+								documentation and execution of repairs.{" "}
+							</li>
+							<li>
+								Communicate with clients over the phone and in person to reveal
+								diagnostic discoveries and make recommendations for protecting
+								their devices and resolving issues.
+							</li>
+							<li>
+								Communicate with clients over the phone and in person to reveal
+								diagnostic discoveries and make recommendations for protecting
+								their devices and resolving issues.
+							</li>
+						</ul>
+					</p>
 				</div>
 			</div>
 		</div>
